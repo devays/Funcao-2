8a20b9fd961d7a6d44cbf34e362be33dc9274064
devays <allysxd@gmail.com>
2022-09-13 15:08:23 -0300

    Funcao Exercícios

---
+++ printComFuncao.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+    print()
+
+
+escreva('Exercícios em Python')
+escreva('Começando Funções')
+escreva('Primeiros Exercícios.')
